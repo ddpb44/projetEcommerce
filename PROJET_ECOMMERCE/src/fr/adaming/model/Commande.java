8aceb54369f5f13bbe0fe309f1a796e9bacf43b9
inti0449 <inti0449@192.168.1.165>
2017-10-25 15:19:44 +0200

    David : réponse à la première salve

--- PROJET_ECOMMERCE/src/fr/adaming/model/Commande.java
+++ PROJET_ECOMMERCE/src/fr/adaming/model/Commande.java
@@ -1,5 +1,90 @@
 package fr.adaming.model;
 
-public class Commande {
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="commandes")
+public class Commande implements Serializable{
+	
+	// Attributs
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id_commande;
+	
+	private Long date_naissance;
+	
+	// Association UML --> JAVA
+	@OneToMany(mappedBy="attCommande", fetch=FetchType.EAGER)
+	private List<LigneCommande> ligneCom;
+	
+	@ManyToOne
+	@JoinColumn(name="client_id", referencedColumnName="id_client")
+	private Client attClient;
+
+	// Constructeur vide
+	public Commande() {
+		super();
+	}
+
+	// Getters/Setters
+
+	public Long getId_commande() {
+		return id_commande;
+	}
+
+	public void setId_commande(Long id_commande) {
+		this.id_commande = id_commande;
+	}
+
+
+	public Long getDate_naissance() {
+		return date_naissance;
+	}
+
+	public void setDate_naissance(Long date_naissance) {
+		this.date_naissance = date_naissance;
+	}
+
+	public List<LigneCommande> getLigneCom() {
+		return ligneCom;
+	}
+
+	public void setLigneCom(List<LigneCommande> ligneCom) {
+		this.ligneCom = ligneCom;
+	}
+
+	public Client getAttClient() {
+		return attClient;
+	}
+
+	public void setAttClient(Client attClient) {
+		this.attClient = attClient;
+	}
+
+	// Méthode toString()
+	@Override
+	public String toString() {
+		return "Commande [id_commande=" + id_commande + ", date_naissance=" + date_naissance + ", ligneCom=" + ligneCom
+				+ ", attClient=" + attClient + "]";
+	}
+
+	
+	
+
+	
+	
+	
 
 }
--- PROJET_ECOMMERCE/src/fr/adaming/model/LigneCommande.java
+++ PROJET_ECOMMERCE/src/fr/adaming/model/LigneCommande.java
@@ -1,5 +1,88 @@
 package fr.adaming.model;
 
-public class LigneCommande {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "lignes_de_commande")
+public class LigneCommande implements Serializable {
+
+	// Attributs
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id_lc;
+
+	private int quantite;
+
+	private int prix;
+
+	// Associations UML --> JAVA
+	@ManyToOne
+	@JoinColumn(name = "produit_id", referencedColumnName = "id_produit")
+	private Produit attProduit;
+
+	@ManyToOne
+	@JoinColumn(name = "commande_id", referencedColumnName = "id_commande")
+	private Commande attCommande;
+
+	// Constructeur vide
+	public LigneCommande() {
+		super();
+	}
+
+	// Getters/Setters
+	public int getId_lc() {
+		return id_lc;
+	}
+
+	public void setId_lc(int id_lc) {
+		this.id_lc = id_lc;
+	}
+
+	public int getQuantite() {
+		return quantite;
+	}
+
+	public void setQuantite(int quantite) {
+		this.quantite = quantite;
+	}
+
+	public int getPrix() {
+		return prix;
+	}
+
+	public void setPrix(int prix) {
+		this.prix = prix;
+	}
+
+	public Produit getAttProduit() {
+		return attProduit;
+	}
+
+	public void setAttProduit(Produit attProduit) {
+		this.attProduit = attProduit;
+	}
+
+	public Commande getAttCommande() {
+		return attCommande;
+	}
+
+	public void setAttCommande(Commande attCommande) {
+		this.attCommande = attCommande;
+	}
+
+	// Méthode toString()
+	@Override
+	public String toString() {
+		return "LigneCommande [id_lc=" + id_lc + ", quantite=" + quantite + ", prix=" + prix + ", attProduit="
+				+ attProduit + ", attCommande=" + attCommande + "]";
+	}
 
 }
