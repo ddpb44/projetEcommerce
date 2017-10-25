111687d09c9ab1945ab0832e69baa59c16ae166d
inti0232 <inti0232@192.168.1.169>
2017-10-25 14:17:38 +0200

    Pablo : Class Admin

--- PROJET_ECOMMERCE/src/fr/adaming/model/Admin.java
+++ PROJET_ECOMMERCE/src/fr/adaming/model/Admin.java
@@ -1,5 +1,61 @@
 package fr.adaming.model;
 
-public class Admin {
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="admins")
+public class Admin implements Serializable {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id_admin")
+	private int id;
+	
+	private String mail;
+	
+	private String mdp;
+
+	public Admin() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getMdp() {
+		return mdp;
+	}
+
+	public void setMdp(String mdp) {
+		this.mdp = mdp;
+	}
+
+	@Override
+	public String toString() {
+		return "Admin [id=" + id + ", mail=" + mail + ", mdp=" + mdp + "]";
+	}
+	
+	
+	
 
 }
